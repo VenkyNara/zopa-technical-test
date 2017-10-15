ec275ebb0e868953566e3a104f9ae87c07889a32
VenkyN <venkaiah.n@gmail.com>
2017-10-15 23:03:43 +0100

    application changes

--- src/main/java/com/zopa/quote/caluclations/client/Application.java
+++ src/main/java/com/zopa/quote/caluclations/client/Application.java
@@ -1,13 +1,38 @@
 package com.zopa.quote.caluclations.client;
 
+import java.util.List;
+
+import com.zopa.quote.caluclations.api.IQuoteCalculatorAPI;
+import com.zopa.quote.caluclations.api.QuoteCalculator;
+import com.zopa.quote.caluclations.model.Offer;
+import com.zopa.quote.caluclations.model.QuoteCalculationRs;
+import com.zopa.quote.caluclations.utils.CSVParser;
+
 /**
- * Hello world!
+ * @author venky
  *
  */
-public class Application 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Application {
+	private static final Integer LOAN_LENGTH_MONTHS = 36;
+
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			throw new IllegalArgumentException("Pass Market data file path and specify the loan amount !!!");
+		}
+
+		String csvFile = args[0];
+		Integer loanAmount = Integer.valueOf(args[1]);
+
+		List<Offer> offers = CSVParser.parseCSV(csvFile);
+
+		IQuoteCalculatorAPI caluclator = new QuoteCalculator();
+		QuoteCalculationRs quoteCalculationRs = caluclator.getQuote(loanAmount, offers, LOAN_LENGTH_MONTHS);
+		if (quoteCalculationRs != null) {
+			System.out.println("Requested amount: " + quoteCalculationRs.getLoanAmount());
+			System.out.println("Rate: " + quoteCalculationRs.getQuote() + "%");
+			System.out.println("Monthly repayment: " + quoteCalculationRs.getMonthlyRepayment());
+			System.out.println("Total repayment: " + quoteCalculationRs.getTotalRepayment());
+		}
+
+	}
 }
